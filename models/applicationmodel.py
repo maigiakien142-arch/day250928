babb16a578d66c526d86f4e289a2c5ba46f69b7d
Dailuzhen <luzhendai1@gmail.com>
2025-10-11 12:03:30 +0800

    test

--- models/applicationmodel.py
+++ models/applicationmodel.py
@@ -12,15 +12,15 @@ class BasicModel(SQLModel):
 
 class UrgencyLevel(str, Enum):
     """紧急程度枚举"""
-    low = "0"  # 低
-    medium = "1"  # 中
-    high = "2"  # 高
+    low = "low"  # 低
+    medium = "medium"  # 中
+    high = "high"  # 高
 
 
 class GPUServer(str, Enum):
     """gpu服务器是否可用"""
-    available = "0"  #可用
-    unavailable = "1"  #不可用
+    available = "available"  #可用
+    unavailable = "unavailable"  #不可用
 
 
 class Application(BasicModel, table=True):
@@ -29,13 +29,13 @@ class Application(BasicModel, table=True):
     urgency_level: UrgencyLevel = Field(default=UrgencyLevel.low, description="紧急程度，0代表低，1代表中，2代表高",index=True)
     application_title: str = Field(description="申请标题", max_length=255)
     expected_completion: datetime = Field(description="预期完成时间")
-    gpu_server: GPUServer = Field(default=UrgencyLevel.low, description="gpu服务器是否可用，0可用，1代表不可用",index=True)
+    gpu_server: GPUServer = Field(default=GPUServer.available, description="gpu服务器是否可用，0可用，1代表不可用",index=True)
     start_number: int = Field(description="起始编号")
     description: str = Field(description="描述", max_length=255)
-    contribution_score: str = Field(description="贡献得分", max_length=50)
-    research_score: str = Field(description="科研得分", max_length=50)
-    efficiency_score: str = Field(description="效率得分", max_length=50)
-    comprehensive_score: str = Field(description="综合得分", max_length=50)
+    contribution_score: float = Field(description="贡献得分", max_length=50)
+    research_score: float = Field(description="科研得分", max_length=50)
+    efficiency_score: float = Field(description="效率得分", max_length=50)
+    comprehensive_score: float = Field(description="综合得分", max_length=50)
     contact_person: str = Field(description="联系人", max_length=50)
     contact_phone: str = Field(description="联系电话", max_length=50)
     contact_email: str = Field(description="联系邮箱", max_length=50)
